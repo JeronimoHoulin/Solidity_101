207dd9c2fe0840f0ef896fef505c77c17d0687ef
JeronimoHoulin <79488175+JeronimoHoulin@users.noreply.github.com>
2021-05-30 10:49:31 -0300

    Update Test_Thesis.sol

--- Test_Thesis.sol
+++ Test_Thesis.sol
@@ -19,17 +19,27 @@ contract contrato_basico {
 
     }
     
+    
     //SUPONGAMOS QUE ENTRAN DOS PERSONAS DE CADA PUNTA PARA EJECUTAR ELCONTRATO
-        long_trader = long_trader(1, 100, false)
-        short_trader = short_trader(1, 100, false)
+        function set_long() public{
+            long_trader = long_trader(1, 100, false);
+        }
+        
+                function set_short() public{
+            short_trader = long_trader(1, 100, false);
+        }
+        
         
     //VAMOS A PONER SU MARGIN EN UN "POTT"
-    uint pot_balance = short_trader[margin] + long_trader[margin]
+    uint pot_balance = short_trader.margin + long_trader.margin;
     
     //CREAMOS UNA SITUACIÓN DONDE UNA DE LAS PARTES DESEA LIQUIDAR SU POSICION
-    if(short_trader[liquidation] = true){
-        uint pnl_short = ((stock_price_1 - stock_price_t2)/stock_price_t2) * short_trader[margin]
-        pot_balance = pot_balance +pnl_short
+    function get_pnl() public view returns (uint){
+        if(short_trader.liquidation = true){
+            uint pnl_short = ((stock_price_t1 - stock_price_t2)/stock_price_t2) * short_trader.margin;
+            
+            pot_balance = pot_balance - short_trader.margin - pnl_short;
+        }
     }
-    
 }
+
